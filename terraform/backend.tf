a060683b4ad881d3220b6f2eaa28ce273653a96e
cgtestuser1 <testuser1@gaajula.com>
2025-10-19 20:08:54 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
+    resource_group_name  = "test"
     storage_account_name = "tfstatestoragecg001"
     container_name       = "tfstatecg"
     key                  = "terraform.tfstate"
